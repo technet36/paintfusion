ab537ee707ba1c02fed5d9f78e9814f2c1a1ced6
technet <ellipse.paro@gmail.com>
2016-02-05 23:56:37 +0100

    cc moy
    fully efficient timeCC from Json

--- mon.js
+++ mon.js
@@ -1,45 +1,55 @@
 //api key : f7b8a452-ec7b-48d0-9681-fcd97a0a4321
 
 $(document).ready(function(){ //quand le document se charge
-    var timeCC=[];
-    var nameChamp=[];
+
+    var Champ = [];
     var idChamp = [];
-    var index;
-    var compteur = [];
-    var y = [];
+    var id ;
+    var compteur = 0;
     $.getJSON("matches1.json", function (data){
         $.each(data.matches,function(i, match){
-            $.each(match.participants,function(i, player){
-                index=$.inArray(player.championId,idChamp);
-            if ( index != -1 ) {
-              //  console.log(nameChamp);
-                //  console.log("length "+idChamp.length);
-                console.log(compteur);
-                compteur[index] = compteur[index]+1;
-                timeCC[index] =(compteur[index]-1)*timeCC[index]+player.stats.totalTimeCrowdControlDealt;
-                //console.log(compteur[index]+"-1*"+timeCC[index]+"+"+player.stats.totalTimeCrowdControlDealt);
-            }
-            else {
-                idChamp.push(player.championId);
-                timeCC.push(player.stats.totalTimeCrowdControlDealt);
-                compteur[compteur.length-1] = 1;
-                $.getJSON("https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion/"+player.championId+"?champData=info&api_key=f7b8a452-ec7b-48d0-9681-fcd97a0a4321", function (data){
-                    nameChamp.push(data.name);
-                    y.push("1");
-                    y.push("2");
-                    y.push("3");
-                });
-            }
-            });
+             $.each(match.participants,function(i, player){
+            id = player.championId;
+               if ($.inArray(id,idChamp) == (-1) ){
+                   idChamp.push(id);
+                   Champ[compteur] = {id : id, timeCC : player.stats.totalTimeCrowdControlDealt, name : "Unknown",count:1};
+                   compteur ++;
+                }
+            else   {
+                   Champ[$.inArray(id,idChamp)].timeCC =((Champ[$.inArray(id,idChamp)].count)*Champ[$.inArray(id,idChamp)].timeCC+player.stats.totalTimeCrowdControlDealt)/((Champ[$.inArray(id,idChamp)].count)+1);
+                   Champ[$.inArray(id,idChamp)].count ++;
+               }
+             });
         });
-        console.log(nameChamp);
-        console.log(y);
-        console.log(nameChamp[2]);
-        console.log(y[2]);
-        console.log(timeCC);
-        $("#table2").append("<p>"+nameChamp+"</p>");
-        $("#table1").append("<p>"+timeCC+"</p>");
+        getName(Champ);
     });
 
-
 });
+
+function getName( tabTotal )
+{
+    var test;
+    //console.log(champId);
+    $.each(tabTotal,function(i,champ){
+        test = $.getJSON("https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion/"+champ.id+"?champData=info&api_key=f7b8a452-ec7b-48d0-9681-fcd97a0a4321", function (data){
+                tabTotal[i].name = data.name;
+                $("#table0").append("<p>"+tabTotal[i].id+"</p>");
+                $("#table1").append("<p>"+tabTotal[i].name+"</p>");
+                $("#table2").append("<p>"+tabTotal[i].timeCC+"</p>");
+                i++;
+            })
+            .done(function() {
+                console.log("succes getName");
+            })
+            .fail(function() {
+                console.log( "error getName" );
+            })
+    });
+
+//console.log(Champ.name);
+    return tabTotal;
+};
+function printChamp (tabChamp){
+    for (var i = 0 ; i<tabChamp.length ; i++){
+    }
+}
